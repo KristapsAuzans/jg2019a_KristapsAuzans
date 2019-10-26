951ec3161f64e0472daf3936994d4ed0326dbb33
KristapsAuzans <kristaps.auzans@gmail.com>
2019-10-27 00:25:32 +0300

    Homework lesson 2

--- src/lv/javaguru/lesson4/Homework4/CreditCartFunctionality.java
+++ src/lv/javaguru/lesson4/Homework4/CreditCartFunctionality.java
@@ -2,7 +2,7 @@ package lv.javaguru.lesson4.Homework4;
 
 public class CreditCartFunctionality {
     public static void main(String[] args) {
-        CreditCard first = new CreditCard(1234, 100, 200, 20);
+        CreditCard first = new CreditCard(1234, 100, 250, 20);
        first.chooseAction();
     }
 }
