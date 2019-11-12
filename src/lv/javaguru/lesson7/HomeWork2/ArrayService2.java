4b5c3b259a06923d0e4c0078f1b966d8609b7d6e
KristapsAuzans <kristaps.auzans@gmail.com>
2019-11-12 22:58:53 +0200

    Homework lesson 7 HW2

---
+++ src/lv/javaguru/lesson7/HomeWork2/ArrayService2.java
@@ -0,0 +1,44 @@
+package lv.javaguru.lesson7.HomeWork2;
+
+import java.util.Random;
+import java.util.Arrays;
+
+public class ArrayService2 {
+    public int[] create(int n) {
+        return new int[n];
+    }
+
+    public void fillArrayWithRandomNumbers(int[] array) {
+        Random randomGenerator = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = randomGenerator.nextInt(101);
+        }
+    }
+
+    public void printArrayToConsole(int[] array) {
+        for (int elementOfArray : array) {
+            System.out.println(elementOfArray);
+        }
+    }
+
+    public void sortArray(int[] array) {
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public void swapArrays(int[] array){
+        int j=0;
+        int[] swappedArray = new int[array.length];
+
+        for(int i = array.length-1; i>=0; i--){
+           swappedArray[j]= array[i];
+           j++;
+        }
+        System.out.println(Arrays.toString(swappedArray));
+    }
+
+
+
+
+
+}
---
+++ src/lv/javaguru/lesson7/HomeWork2/ArrayServiceTest2.java
@@ -0,0 +1,14 @@
+package lv.javaguru.lesson7.HomeWork2;
+
+public class ArrayServiceTest2 {
+    public static void main(String[] args) {
+        ArrayService2 first = new ArrayService2();
+        int[] array = first.create(10);
+        first.fillArrayWithRandomNumbers(array);
+        first.printArrayToConsole(array);
+        first.sortArray(array);
+        first.swapArrays(array);
+
+    }
+
+}
